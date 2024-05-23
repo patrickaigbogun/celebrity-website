11e90d8d42ae0c2566123c9b742a7ad5a0deaf27
youngboypimpin <rjtekdom@gmail.com>
2024-05-23 18:06:32 +0100

    added annotation to footer

--- app/landing/components/templates/footer.tsx
+++ app/landing/components/templates/footer.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Footer() {
 	return <footer className="footer mt-auto py-3 bg-light">
 	<div className="container">
-	  <span className="text-muted">Place sticky footer content here.</span>
+	  
+	  <p>Built by Patrick Otasowie Aigbogun founder of Oti.</p> 
+	  <span className="text-muted">&copy;Oti.</span>
 	</div>
   </footer>
   ;

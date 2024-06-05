1189d62e685c6e054ee874a657818622ec7f7be9
youngboypimpin <rjtekdom@gmail.com>
2024-06-05 20:29:05 +0100

    added footer content

--- app/landing/components/templates/footer.tsx
+++ app/landing/components/templates/footer.tsx
@@ -4,7 +4,10 @@ export default function Footer() {
 	return (
 		<footer className="footer mt-auto py-3 bg-light">
 			<div className="container">
-				<p>Built by Patrick Otasowie Aigbogun founder of Oti.</p>
+				<p className="fs-6 fw-light" >
+				This is a private meeting different from public appearance, It is expected of you that this private meeting is kept private within and only to yourself, do not reveal location to anyone for security reasons and to avoid unnecessary crowed, media personalities/journalists, gift presentations and autograph signing
+				</p>
+				<p>Built by Patrick Otasowie Aigbogun, founder of Oti.</p>
 				<span className="text-muted">&copy;Oti.</span>
 			</div>
 		</footer>

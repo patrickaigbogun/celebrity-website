cf511409642d3fc951da415c1482df4365dbe013
youngboypimpin <rjtekdom@gmail.com>
2024-06-05 19:59:05 +0100

    vertical margin - Contactus section

--- app/landing/components/sections/Contactus.tsx
+++ app/landing/components/sections/Contactus.tsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function ContactUs() {
 	return (
 		<section
-			className="row pt-5 container-fluid justify-content-center px-3"
+			className="row pt-5 container-fluid justify-content-center px-3 my-5"
 			id="contact">
 			<div
 				className="col-12 col-md-9 col-lg-8 border rounded-2 shadow p-4 bg-light"
 				style={{ boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)" }}>
 				<h1 className="display-7 text-center mb-4 fw-bold">
-					Contact & Booking
+					Contact &amp; Booking
 				</h1>
 				<p className="text-center">
 				Fill in your information down below and directly message the private telegram with payment proof when done for successful meet up booking

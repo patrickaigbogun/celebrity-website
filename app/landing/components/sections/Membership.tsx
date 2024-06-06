9d0d5a7963deafd5e35f5f1ca7239aaf9c7d2386
youngboypimpin <rjtekdom@gmail.com>
2024-06-06 13:56:29 +0100

    n

--- app/landing/components/sections/Membership.tsx
+++ app/landing/components/sections/Membership.tsx
@@ -3,24 +3,21 @@ import Checkout from "./Checkout";
 
 export default function Fanclub() {
 	return (
-		<section className="home container-md mb-4 pt-5 my-5" id="membership">
+		<section className="home container-md my-5 pt-5" id="membership">
 			<svg xmlns="http://www.w3.org/2000/svg" className="svg">
 				<symbol id="check" viewBox="0 0 16 16">
 					<title>Check</title>
 					<path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
 				</symbol>
 			</svg>
-			<h1 className="display-6 text-center mb-4 fw-bold">
+			<h1 className="display-6 text-center my-4 fw-bold">
 				Membership Access
-			</h1>{" "}
-			<br />
-			<br />
-			<div className=" row py-0 mb-3 text-center ">
+			</h1>
+			<div className=" row py-0 my-4 text-center ">
 				<div className="col-md-6 h-100 py-0">
 					<div className="card mb-4 rounded-3 shadow h-100 py-0">
 						<div className="card-header py-3">
-							<h4 className="fw-semibold">VIP PREMIUM
-</h4>
+							<h4 className="fw-semibold">VIP PREMIUM</h4>
 						</div>
 						<div className="card-body">
 					
@@ -62,12 +59,10 @@ export default function Fanclub() {
 					</div>
 				</div>
 			</div>
-			<br />
-			<br />
+			
 			<Checkout />
-			<br />
-			<br />
-			<h2 className="display-6 text-center my-3 fw-semibold">
+			
+			<h2 className="display-6 text-center my- 5 fw-semibold">
 			Meetup Perks
 			</h2>
 

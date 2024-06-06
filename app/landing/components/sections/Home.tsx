b55130c039dff7b53b951550cd5ee5c9bbe245df
youngboypimpin <rjtekdom@gmail.com>
2024-06-06 13:24:12 +0100

    modified home content

--- app/landing/components/sections/Home.tsx
+++ app/landing/components/sections/Home.tsx
@@ -1,32 +1,38 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 type Props = {};
 export default function Home({}: Props) {
 	return (
-		<section className="px-4 pt-0 my-1 text-center" id="home">
-			<div className="px-4 py-5 my-5 text-center">
-				<Image
+		<section className="px-4 pt-0 my-1" id="home">
+			<div className="px-4 py-5 my-5">
+				{/* <Image
 					className="d-block mx-auto mb-4"
 					src="/logo-dark-wbg.svg"
 					alt="Your logo"
 					width={150}
 					height={100}
-				/>
-				<h1 className="display-5 fw-bold">Home</h1>
-				<div className="col-lg-6 mx-auto">
-					<p className="mb-4 fs-4 fw-4">
-					Message my private telegram page or speak to my manager to talk more with me
-					</p>
+				/> */}
+				
+				<div className="container mx-auto">
+
+					<h1 className=" display-5 text-center fw-bold my-2">Page Title</h1>
+					<p className="my-5 text-center fs-2" >Message my private telegram page or speak to my manager to talk more with me</p>
+					<p className=" lead mt-0 mb-3 text-center fs-5">
+				This platform was created by my team , 
+				To reach out to my great fans that has been with me and supported me through my career,
+				 couldn’t find away to appreciate you all for following me through this journey this is my 
+				 official page for reaching out to my team and I personally
+				</p>
 					<div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
 						<button
 							type="button"
 							className=" shadow btn btn-dark btn-lg px-4 gap-3">
-							Explore
+							<a className="text-decoration-none text-reset" href="#contact">Send a Message</a>
 						</button>
 						<button
 							type="button"
 							className="shadow btn btn-outline-dark btn-lg gap-3 px-4">
-							Telegram
+							<a className="text-decoration-none text-reset" href="#">Telegram</a>
 							<svg className="bi" width="24" height="24">
 								<use xlinkHref="#telegram" />
 							</svg>
